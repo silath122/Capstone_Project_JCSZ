c0eb9fa1243776151ed6188150813a6470c7b2b9
Siah Thomas <thomassl1@g.cofc.edu>
2024-03-25 19:52:05 -0400

    add_edge_device.html updated - still issues with provider input dropdown

--- public/firebase.js
+++ public/firebase.js
@@ -44,33 +44,87 @@ async function updateLastUpdateTime() {
   await metadataRef.set({ lastUpdate: firebase.firestore.FieldValue.serverTimestamp() });
 }
 
+
 // Form submission event listener
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('deviceForm').addEventListener('submit', function (event) {
+    document.getElementById('deviceForm').addEventListener('submit', async function (event) {
         event.preventDefault();
         var deviceName = document.getElementById('deviceName').value.trim();
         var deviceProvider = document.getElementById('deviceProvider').value.trim();
+        var deviceProductSpecs = document.getElementById('deviceProductSpecs').value.trim();
 
-        if (deviceName === '' || deviceProvider === '') {
-            alert('Please fill in both device type and device software.');
+
+
+        if (deviceName === '' || deviceProvider === '' || deviceProductSpecs == '') {
+            alert('Please fill in both device type, device software, and the device product specification link.');
             return;
         }
 
-        var dataToAdd = {
-            name: deviceName,
-            image: deviceName,
-            provider: deviceProvider
-        };
-
-        db.collection('edge-devices').doc(deviceName).set(dataToAdd)
-            .then(function () {
-                console.log('Document successfully written!');
-                updateLastUpdateTime(); // Update last update timestamp
-            })
-            .catch(function (error) {
-                console.error('Error adding document: ', error);
+        try {
+            // Add the device document to edge-devices collection
+            await db.collection('edge-devices').doc(deviceName).set({
+                name: deviceName,
+                provider: deviceProvider,
+                productSpecsLink: deviceProductSpecs
             });
+
+            var certifications = [];
+            var certificationInputs = document.querySelectorAll('.certification');
+            certificationInputs.forEach(function(certInput) {
+                var certificationProductName = certInput.querySelector('input[name="certificationProductName"]').value.trim();
+                var certificationArchitecture = certInput.querySelector('input[name="certificationArchitecture"]').value.trim();
+                if (certificationProductName !== '' && certificationArchitecture !== '') {
+                    certifications.push({ product: certificationProductName, architecture: certificationArchitecture });
+                }
+            });
+        
+
+            // Add certifications to the sub-collection under the device document
+            var certificationsCollection = db.collection('edge-devices').doc(deviceName).collection('certifications');
+            certifications.forEach(async function(certification) {
+                await certificationsCollection.doc(certification.product).set(certification);
+            });
+        
+            // Clear input fields
+            document.getElementById('deviceName').value = '';
+            document.getElementById('deviceProvider').value = '';
+            document.getElementById('deviceProductSpecs').value = '';
             
+            // Remove additional certification input fields
+            var certificationInputs = document.querySelectorAll('.certification');
+            for (var i = 1; i < certificationInputs.length; i++) {
+                certificationInputs[i].remove();
+            }
+
+
+            // Display success message
+            alert('Edge device has been successfully added to Firestore!');
+            console.log('Device and certifications successfully added to Firestore!');
+            updateLastUpdateTime();
+        } catch (error) {
+            // Display error message
+            alert('Error: Edge device could not be added.');
+            console.error('Error adding document: ', error);
+        }
+            
+    });
+
+    // An 'add more' button event listener for form submission certifications
+    document.getElementById('addCertification').addEventListener('click', function () {
+        var certificationsDiv = document.getElementById('certifications');
+        var newCertificationDiv = document.createElement('div');
+
+        newCertificationDiv.classList.add('certification');
+        newCertificationDiv.innerHTML = `
+            <input type="text" class="form-control" name="certificationProductName" placeholder="Enter Product Certification Name">
+            <input type="text" class="form-control" name="certificationArchitecture" placeholder="Enter Architecture Certification Name">
+        `;
+        
+        // Add space between inputs
+        newCertificationDiv.style.marginTop = '10px';
+        newCertificationDiv.style.marginBottom = '10px';
+
+        certificationsDiv.insertBefore(newCertificationDiv, document.getElementById('addCertification'));
     });
 
     // Search devices event listener
@@ -86,16 +140,62 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// Fetch edge devices from Firestore or cache
+
+// Fetch providers from Firestore and populate dropdown for add_edge_device.html page
+async function fetchProvidersAddEdge() {
+    var providerDropdownMenu = document.getElementById("providerDropdownMenu");
+    if (!providerDropdownMenu) return;
+
+    try {
+        var snapshot = await db.collection("provider-image-links").get();
+        snapshot.forEach((doc) => {
+            var providerName = doc.data().provider;
+            var listItem = document.createElement("li");
+            var anchor = document.createElement("a");
+            anchor.href = "#";
+            anchor.textContent = providerName;
+            anchor.dataset.provider = providerName; // Store provider name as a dataset attribute
+            listItem.appendChild(anchor);
+            providerDropdownMenu.appendChild(listItem);
+        });
+
+        // Add event listener to the dropdown menu to handle item clicks
+        providerDropdownMenu.addEventListener('click', function(event) {
+            var selectedProvider = event.target.dataset.provider; // Retrieve provider name from dataset attribute
+            if (selectedProvider) {
+                console.log("Selected provider:", selectedProvider); // Log selected provider
+                document.getElementById("providerDropdown").innerText = selectedProvider; // Update button text
+            }
+        });
+
+    } catch (error) {
+        console.error("Error fetching providers: ", error);
+    }
+
+    console.log("Fetching provider data...");
+    try {
+        var snapshot = await db.collection("provider-image-links").get();
+        var providerNames = [];
+        snapshot.forEach((doc) => {
+            var providerName = doc.id;
+            providerNames.push(providerName); // Store provider names
+        });
+        console.log("Provider names:", providerNames); // Log retrieved provider names
+    } catch (error) {
+        console.error("Error fetching providers: ", error);
+    }
+}
+
+
 // Fetch edge devices from Firestore or cache
 async function fetchEdgeDevices() {
   var container = document.getElementById("edge-devices-container");
   if (!container) return;
 
-  container.innerHTML = ''; // Clear previous content
+  container.innerHTML = ''; // clears previous content
 
   var devices = [];
-  var cacheLastUpdate = localStorage.getItem("lastUpdateTime");
+  var cacheLastUpdate = localStorage.getItem("lastUpdateTime"); // see when was the last time data was added
   var metadataRef = db.collection("metadata").doc("lastUpdate");
 
   try {
@@ -250,8 +350,8 @@ function searchDevices() {
     }
 }
 
-// Call fetchProviders after the document is loaded
 document.addEventListener('DOMContentLoaded', function () {
+    fetchProvidersAddEdge();
     fetchEdgeDevices();
     fetchProviders();
 });
